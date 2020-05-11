96d8da4494c98a46212ce2d5053c0f51a1929faf
caoguangqiang <15295377665@163.com>
2020-05-11 22:30:19 +0800

    添加类型注释

--- rentomatic/use_cases/request_objects.py
+++ rentomatic/use_cases/request_objects.py
@@ -1,21 +1,22 @@
-import collections
+import collections.abc
+from typing import Dict, Optional
 
 from rentomatic.shared import request_object as req
 
-
 class StorageRoomListRequestObject(req.ValidRequestObject):
 
-    def __init__(self, filters=None):
+    def __init__(self, filters: Optional[dict] = None) -> None:
         self.filters = filters
 
     @classmethod
-    def from_dict(cls, adict):
+    def from_dict(cls, adict: Dict[str, dict]):
         invalid_req = req.InvalidRequestObject()
 
         if 'filters' in adict and not isinstance(adict['filters'], collections.Mapping):
             invalid_req.add_error('filters', 'Is not iterable')
 
         if invalid_req.has_errors():
+            # type: req.InvalidRequestObject
             return invalid_req
 
         return StorageRoomListRequestObject(filters=adict.get('filters', None))
