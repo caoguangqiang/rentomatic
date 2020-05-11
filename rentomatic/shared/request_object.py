9e1ee0730f29a5c5f7d1377264382fc5ccf507c8
caoguangqiang <15295377665@163.com>
2020-05-11 16:35:45 +0800

    添加注释类型

--- rentomatic/shared/request_object.py
+++ rentomatic/shared/request_object.py
@@ -1,12 +1,18 @@
-class InvalidRequestObject(object):
+from typing import List, Dict, Any
+
+class RequestObject():
+    def __nonzero__(self):
+        raise NotImplementedError
+
+class InvalidRequestObject(RequestObject):
 
     def __init__(self):
-        self.errors = []
+        self.errors: List[dict] = []
 
     def add_error(self, parameter, message):
         self.errors.append({'parameter': parameter, 'message': message})
 
-    def has_errors(self):
+    def has_errors(self) -> bool:
         return len(self.errors) > 0
 
     def __nonzero__(self):
@@ -15,10 +21,10 @@ class InvalidRequestObject(object):
     __bool__ = __nonzero__
 
 
-class ValidRequestObject(object):
+class ValidRequestObject(RequestObject):
 
     @classmethod
-    def from_dict(cls, adict):
+    def from_dict(cls, adict:Dict[str, Any]):
         raise NotImplementedError
 
     def __nonzero__(self):
