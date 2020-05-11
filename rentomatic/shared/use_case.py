b6785ab13c62329a7ec01384fb5b38b551a9a282
caoguangqiang <15295377665@163.com>
2020-05-11 16:50:08 +0800

    添加注释类型

--- rentomatic/shared/use_case.py
+++ rentomatic/shared/use_case.py
@@ -1,9 +1,11 @@
-from rentomatic.shared import response_object as res
+from typing import Union, NoReturn
 
+from rentomatic.shared import response_object as res
+from rentomatic.shared import request_object as req
 
 class UseCase(object):
 
-    def execute(self, request_object):
+    def execute(self, request_object: req.InvalidRequestObject) -> res.Response:
         if not request_object:
             return res.ResponseFailure.build_from_invalid_request_object(request_object)
         try:
@@ -12,6 +14,6 @@ class UseCase(object):
             return res.ResponseFailure.build_system_error(
                 "{}: {}".format(exc.__class__.__name__, "{}".format(exc)))
 
-    def process_request(self, request_object):
+    def process_request(self, request_object)-> res.ResponseSuccess:
         raise NotImplementedError(
             "process_request() not implemented by UseCase class")
