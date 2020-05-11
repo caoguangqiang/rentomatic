f0aa86c4da90098a1a3e286fa404c5c30fc039fc
caoguangqiang <15295377665@163.com>
2020-05-11 22:35:53 +0800

    添加 type annotation

--- rentomatic/use_cases/storageroom_use_cases.py
+++ rentomatic/use_cases/storageroom_use_cases.py
@@ -1,12 +1,17 @@
+from typing import List
+
+from rentomatic.use_cases.request_objects import StorageRoomListRequestObject
+from rentomatic.shared import storageropm_repo as repo
+from rentomatic.domain.storageroom import StorageRoom
 from rentomatic.shared import use_case as uc
 from rentomatic.shared import response_object as res
 
 
 class StorageRoomListUseCase(uc.UseCase):
 
-    def __init__(self, repo):
+    def __init__(self, repo: repo.StorageRoomRepo) -> None:
         self.repo = repo
 
-    def process_request(self, request_object):
-        domain_storageroom = self.repo.list(filters=request_object.filters)
+    def process_request(self, request_object: StorageRoomListRequestObject) -> res.ResponseSuccess:
+        domain_storageroom: List[StorageRoom] = self.repo.list(filters=request_object.filters)
         return res.ResponseSuccess(domain_storageroom)
